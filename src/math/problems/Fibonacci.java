e76acc76b3053e600a83df5a33c454c7028a8150
fshah <farihashah1010@gmail.com>
2019-09-08 13:42:58 -0400

    Finished Fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -2,6 +2,16 @@ package math.problems;
 
 public class Fibonacci {
     public static void main(String[] args) {
+
+        int num1 = 0;
+        int num2 = 1;
+        for (int i=0; i<=40; i++ ){
+            System.out.println(num1);
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+
+
+        }
          /*
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
