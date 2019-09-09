2139f898c45dd2ec40f3b0bfd13b62e1ae21960a
fshah <farihashah1010@gmail.com>
2019-09-09 15:22:05 -0400

    finished Make Pyramid

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -3,17 +3,27 @@ package math.problems;
 public class MakePyramid {
     public static void main(String[] args) {
 
+
+        for (int i=1; i<=50; i++){
+            for( int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+
+
         /*   Implement a large Pyramid of stars in the screen with java.
 
-                              *
-                             * *
-                            * * *
-                           * * * *
-                          * * * * *
-                         * * * * * *
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
 
-        */
+         */
 
 
-        }
-  }
+    }}
